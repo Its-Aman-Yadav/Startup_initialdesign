fba782c1d24a5a8ba468e0501e098724e36b9be6
Stanzin7 <109467334+Stanzin7@users.noreply.github.com>
2023-06-29 14:04:49 +0530

    Updated JS file

---
+++ script.js
@@ -0,0 +1,123 @@
+$(document).ready(function () {
+  $(window).scroll(function () {
+    // sticky navbar on scroll script
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
+    }
+
+    // scroll-up button show/hide script
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
+    }
+  });
+
+  // slide-up script
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
+    // removing smooth scroll on slide-up button click
+    $("html").css("scrollBehavior", "auto");
+  });
+
+  $(".navbar .menu li a").click(function () {
+    // applying again smooth scroll on menu items click
+    $("html").css("scrollBehavior", "smooth");
+  });
+
+  // toggle menu/navbar script
+  $(".menu-btn").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
+
+  // typing text animation script
+  var typed = new Typed(".typing", {
+    strings: ["Healthy", "Lifestyle"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
+
+  var typed = new Typed(".typing-2", {
+    strings: ["Cherker", "Seabuckthorn"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
+
+  // owl carousel script
+  $(".carousel").owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplay: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      600: {
+        items: 2,
+        nav: false,
+      },
+      1000: {
+        items: 3,
+        nav: false,
+      },
+    },
+  });
+});
+
+function bondPeriod() {
+  window.location.href = "./bondPeriod.html";
+}
+function week1() {
+  window.location.href = "./week1.html";
+}
+function week2() {
+  window.location.href = "./week2.html";
+}
+function week3() {
+  window.location.href = "./week3.html";
+}
+function week4() {
+  window.location.href = "./week4.html";
+}
+function week5() {
+  window.location.href = "./week5.html";
+}
+function week6() {
+  window.location.href = "./week6.html";
+}
+function week7() {
+  window.location.href = "./week7.html";
+}
+function week8() {
+  window.location.href = "./week8.html";
+}
+function week9() {
+  window.location.href = "./week9.html";
+}
+function week10() {
+  window.location.href = "./week10.html";
+}
+function week11() {
+  window.location.href = "./week11.html";
+}
+function week12() {
+  window.location.href = "./week12.html";
+}
+
+// Landing page design
+function imgSlider(anything) {
+  document.querySelector(".starbucks").src = anything;
+}
+
+function changeCircleColor(color) {
+  const circle = document.querySelector(".circle");
+  circle.style.background = color;
+}
